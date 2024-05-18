e6065e76c99430a7f95159f9dd415306310e382c
codesimplifier <codesimplifier@gmail.com>
2024-05-18 14:55:11 +0530

    v0.1.1 - Bug fix

---
+++ app/fonts.tsx
@@ -0,0 +1,5 @@
+import { Quicksand, Oswald } from "next/font/google"
+
+export const primary = Quicksand({ weight: "500", subsets: ["latin"] });
+export const bold_primary = Quicksand({ weight: "700", subsets: ["latin"] });
+export const secondary = Oswald({ weight: "400", subsets: ["latin"] });
---
+++ constants/index.ts
@@ -0,0 +1,103 @@
+import {
+  Info,
+  GraduationCap,
+  BookA,
+  Microscope,
+  UsersRound,
+  LibraryBig,
+  School,
+} from "lucide-react";
+
+export const navLinks = [
+  {
+    id: 0,
+    title: "about us",
+    icon: Info,
+    sublinks: [
+      "overview",
+      "management",
+      "administration",
+      "vision",
+      "core values",
+      "infrastructure",
+    ],
+  },
+  {
+    id: 1,
+    title: "academics",
+    icon: GraduationCap,
+    sublinks: [
+      "programmes",
+      "ciricullum",
+      "council",
+      "results",
+      "examination",
+      "calendar",
+    ],
+  },
+  {
+    id: 3,
+    title: "admissions",
+    icon: BookA,
+    sublinks: [
+      "information",
+      "courses",
+      "eligibility",
+      "prospects",
+      "fee structure",
+      "helpline",
+    ],
+  },
+  {
+    id: 4,
+    title: "departments",
+    icon: LibraryBig,
+    sublinks: [
+      "information",
+      "courses",
+      "eligibility",
+      "prospects",
+      "fee structure",
+      "helpline",
+    ],
+  },
+  {
+    id: 5,
+    title: "student support",
+    icon: UsersRound,
+    sublinks: [
+      "information",
+      "courses",
+      "eligibility",
+      "prospects",
+      "fee structure",
+      "helpline",
+    ],
+  },
+  {
+    id: 6,
+    title: "Campus",
+    icon: School,
+    sublinks: [
+      "information",
+      "courses",
+      "eligibility",
+      "prospects",
+      "fee structure",
+      "helpline",
+    ],
+  },
+  {
+    id: 7,
+    title: "research",
+    icon: Microscope,
+    sublinks: [
+      "information",
+      "courses",
+      "eligibility",
+      "prospects",
+      "fee structure",
+      "helpline",
+    ],
+  },
+];
